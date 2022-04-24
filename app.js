a91359e8a74cee39a62051c1c752e51df8f5159e
milessteamaccount <ronaldlamdev@gmail.com>
2022-04-23 23:50:50 -0500

    Finished app.js: for getting Advice API

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
+let myQuote = '';
+const getAdviceBtn = document.getElementById('getAdviceBtn');
 
+// Get quote from API and Slip ID from API
 
+async function getQuote() {
+  let quote = await fetch('https://api.adviceslip.com/advice');
+  return quote.json();
+}
+
+// Fill in ID container and blockquote
+
+async function fillQuote() {
+  myQuote = await getQuote();
+  document.getElementById('slip-id').innerHTML = myQuote.slip.id;
+  document.getElementById('advice').innerHTML = '"'+myQuote.slip.advice+'"';
+}
+
+// Advice Button Function
+getAdviceBtn.addEventListener('click', fillQuote);
+  window.onload = () => {
+    fillQuote();
+  };
